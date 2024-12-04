dea3574d5f265bb17641f099707c68242dbdfe20
pappubishwas <pappuovi8@gmail.com>
2024-12-04 16:16:35 +0530

    updated

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookStoreComponent } from './book-store/book-store.component';
 import { SharedModule } from '../shared/shared.module';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
+import { ReturnBookComponent } from './return-book/return-book.component';
 
 
 
 @NgModule({
   declarations: [
-    BookStoreComponent
+    BookStoreComponent,
+    MaintenanceComponent,
+    ReturnBookComponent
   ],
   imports: [
     CommonModule,
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,19 +3,18 @@ import { UserOrdersComponent } from './user-orders/user-orders.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProfileComponent } from './profile/profile.component';
 import { CommonModule } from '@angular/common';
-// import { ApprovalRequestsComponent } from './approval-requests/approval-requests.component';
-// import { AllOrdersComponent } from './all-orders/all-orders.component';
-// import { ViewUsersComponent } from './view-users/view-users.component';
+import { ApprovalRequestsComponent } from './approval-requests/approval-requests.component';
+import { AllOrdersComponent } from './all-orders/all-orders.component';
+import { ViewUsersComponent } from './view-users/view-users.component';
 
 @NgModule({
   declarations: [
     UserOrdersComponent,
     ProfileComponent,
-    // ApprovalRequestsComponent,
-    // AllOrdersComponent,
-    // ViewUsersComponent,
+    ApprovalRequestsComponent,
+    AllOrdersComponent,
+    ViewUsersComponent,
   ],
-  imports: [CommonModule,SharedModule],
-  
+  imports: [CommonModule, SharedModule],
 })
 export class UsersModule {}
